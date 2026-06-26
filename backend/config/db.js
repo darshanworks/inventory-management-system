dcb6b26ffe023651242357690a192182ad7baaa2
Darshan Malagonvkar <darshan08m@gmail.com>
2026-06-26 19:06:11 +0530

    Use environment variables for database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,17 @@
-const mysql = require("mysql2");
 require("dotenv").config();
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
   if (err) {
-    console.log("Database connection failed");
+    console.log("Database Connection Failed", err);
   } else {
     console.log("MySQL Connected");
   }
